c13d0150c61c4b1877526ca674b0f9d7c4c8e46a
petetnt <pete.a.nykanen@gmail.com>
2016-12-14 14:26:26 +0200

    Everything is dry
    Signed-off-by: petetnt <pete.a.nykanen@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: "motley",
+  rules: {
+    "newline-per-chained-call":  ["off"] 
+  }
+}
--- fetch-sites.js
+++ fetch-sites.js
@@ -1,96 +1,44 @@
-var casper = require('casper').create({
+const casper = require("casper").create({ // eslint-disable-line
   pageSettings: {
-    loadImages:  false,
+    loadImages: false,
     loadPlugins: false,
-  }
+  },
 });
 
-var fs = require('fs');
+const fecha = require("fecha");
 
-var urls = {
+const fs = require("fs");
+
+const urls = params => ({
   kanttiini: "http://www.smarteat.fi/menu-pitskun-kanttiini/",
   variantti: "https://www.taitotalo.com/fi/ravintolat/lounaslistat/variantti-viikon-lounaslista/",
   blancco: "http://www.ravintolablancco.com/lounaviikko/pitajanmaki/",
   factory: "http://www.ravintolafactory.com/lounasravintolat/ravintolat/helsinki-pitajanmaki/",
-}
-
-var date = new Date();
-var yyyy = new Date().getFullYear();
-var mm = String(parseInt(new Date().getMonth(), 10) + 1);
-var dd = new Date().getDate().toString();
-
-if (mm.length < 1) {
-  mm = "0" +  mm;
-}
-
-if (dd.length < 1) {
-  dd = "0" +  dd;
-}
-
-var tasteParams = yyyy + "/" + mm + "/" + dd;
-
-urls.taste = "http://www.sodexo.fi/ruokalistat/output/weekly_html/6095/"+tasteParams+"/fi";
+  taste: `http://www.sodexo.fi/ruokalistat/output/weekly_html/6095/${params}/fi`,
+});
 
 const selectors = {
   blancco: ".entry-content",
   kanttiini: ".siteorigin-widget-tinymce.textwidget",
   variantti: ".lunch-list",
   taste: "h1",
-  factory: '.post_content .col-xs-12',
-}
+  factory: ".post_content .col-xs-12",
+};
 
-// TODO: Make a map and loop these to keep DRY
-casper.start(urls.kanttiini);
-casper.waitForSelector(selectors.kanttiini, function() {
-  const html = this.getHTML(selectors.kanttiini, true);
-  fs.write("./static/crawled/kanttiini.html", replaceAllTags(html), 'w');
-  this.echo('Kanttiini OK');
-});
+const tasteParams = fecha.format(new Date(), "yyyy/mm/dd");
 
-casper.thenOpen(urls.variantti);
-casper.waitForSelector(selectors.variantti, function() {
-  const html = this.getHTML(selectors.variantti, true);
-  fs.write("./static/crawled/variantti.html", replaceAllTags(html), 'w');
-  this.echo('Variantti OK');
-});
+const replaceAllTags = (html) => {
+  const divOpen = "<div";
+  const divClose = "</div>";
 
-casper.thenOpen(urls.blancco);
-casper.waitForSelector(selectors.blancco, function() {
-  const html = this.getHTML(selectors.blancco, true);
-  fs.write("./static/crawled/blancco.html", replaceAllTags(html), 'w');
-  this.echo('Blancco OK');
-});
+  const spanOpen = "<span";
+  const spanClose = "</span>";
 
-casper.thenOpen(urls.factory);
-casper.waitForSelector(selectors.factory, function() {
-  const html = this.getHTML(selectors.factory, true);
-  fs.write("./static/crawled/factory.html", replaceAllTags(html), 'w');
-  this.echo('Factory OK');
-});
-
-casper.thenOpen(urls.taste);
-casper.waitForSelector(selectors.taste, function() {
-  var elements = this.getElementsInfo('table');
-  fs.write("./static/crawled/taste.html", replaceAllTags(elements[0].html) + replaceAllTags(elements[1].html), 'w');
-  this.echo('Taste OK');
-});
-
-casper.run(function(){
-  this.echo('Crawling done!').exit();
-});
-
-var replaceAllTags = function(html) {
-  var divOpen = "<div";
-  var divClose = "</div>";
-
-  var spanOpen = "<span";
-  var spanClose = "</span>";
-
-  return html = html
-    .replace(/class\=/gi, 'removed\=')
-    .replace(/style\=/gi, 'removed\=')
-    //.replace(/<br(\s+)?(\/)?(\s+)?>/gi, '')
-    .replace(/<h1/gi, divOpen).replace(/<\/h1>/gi, '')
+  return html
+    .replace(/class=/gi, "removed=")
+    .replace(/style=/gi, "removed=")
+    // .replace(/<br(\s+)?(\/)?(\s+)?>/gi, '')
+    .replace(/<h1/gi, divOpen).replace(/<\/h1>/gi, "")
     .replace(/<h2/gi, divOpen).replace(/<\/h2>/gi, divClose)
     .replace(/<h3/gi, divOpen).replace(/<\/h3>/gi, divClose)
     .replace(/<h4/gi, divOpen).replace(/<\/h4>/gi, divClose)
@@ -106,8 +54,26 @@ var replaceAllTags = function(html) {
     .replace(/<p/gi, divOpen).replace(/<\/p>/gi, divClose)
     .replace(/<ul/gi, divOpen).replace(/<\/ul>/gi, divClose)
     .replace(/<li/gi, divOpen).replace(/<\/li>/gi, divClose)
-    .replace(/<b>/gi, spanOpen+">").replace(/<\/b>/gi, spanClose)
+    .replace(/<b>/gi, `${spanOpen}>`).replace(/<\/b>/gi, spanClose)
     .replace(/<i/gi, spanOpen).replace(/<\/i>/gi, spanClose)
     .replace(/<strong/gi, spanOpen).replace(/<\/strong>/gi, spanClose)
-    .replace(/<em/gi, spanOpen).replace(/<\/em>/gi, spanClose)
-}
+    .replace(/<em/gi, spanOpen).replace(/<\/em>/gi, spanClose);
+};
+
+casper.start().each(Object.keys(urls), (self, key) => {
+  const url = urls(tasteParams)[key];
+  self.thenOpen(url, function run() {
+    if (key !== "taste") {
+      const html = this.getHTML(selectors[key], true);
+      fs.write(`./static/crawled/${key}.html`, replaceAllTags(html), "w");
+    } else {
+      const elements = this.getElementsInfo("table");
+      fs.write(`./static/crawled/${key}.html`, replaceAllTags(elements[0].html) + replaceAllTags(elements[1].html), "w");
+    }
+    this.echo(`${key} OK`);
+  });
+});
+
+casper.run(function run() {
+  this.echo("Crawling done!").exit();
+});
